87b623edbcf0299cfd92fbb8fe77dedeb3eb3bba
Yu-Che Wang <yuche@yuche-Air.localdomain>
2022-05-11 22:43:16 -0700

    Single Linked List Implementation

---
+++ data-structure/linked_list/linked_list.py
@@ -0,0 +1,59 @@
+from node import LinkedListNode
+
+class SingleLinkedList:
+    def __init__(self, root = None):
+        self.root = root
+        self.size = 0
+    
+    def add(self,data):
+        new_node = LinkedListNode(data, self.root)
+        self.root = new_node
+        self.size += 1
+
+    def find(self,data):
+        cur = self.root
+        while cur is not None:
+            if cur.data == data:
+                return data
+            else:
+                cur = cur.next
+        return None
+
+    def remove(self,data):
+        cur = self.root
+        prev = None
+        while cur is not None:
+            if cur.data == data:
+                if prev is not None:
+                    prev.next = cur.next
+                else:
+                    self.root = cur.next
+
+                self.size-=1
+                return True
+            else:
+                prev = cur
+                cur = cur.next
+        return False
+
+    def print_list(self):
+        cur = self.root
+        while cur is not None:
+            print(cur.data, end="->")
+            cur = cur.next
+        print('None')
+
+
+if __name__ == "__main__":
+    l = SingleLinkedList()
+    l.add(5)
+    l.add(6)
+    l.add(8)
+    l.add(12)
+    l.print_list()
+
+    print("size:" + str(l.size))
+    l.remove(8)
+    print("size:" + str(l.size))
+    print(l.find(5))
+    print(l.root)
